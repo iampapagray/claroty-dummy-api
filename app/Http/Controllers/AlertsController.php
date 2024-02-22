7a4dd56d1e17833f665046fa4840487980b2dabb
John Graham <iampapagray@gmail.com>
2024-02-22 21:56:58 +0000

    App Complete

---
+++ app/Http/Controllers/AlertsController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Alerts;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Str;
+use ClaudioDekker\WordGenerator\Generator;
+use DateTime;
+use stdClass;
+
+class AlertsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = [];
+        $status = ["Resolved", "Unresolved"];
+        /***
+
+      "category": "Segmentation",
+      "detected_time": "2021-07-11T19:40:46.835404+00:00",
+      "updated_time": "2021-07-11T19:40:46.835404+00:00",
+      "devices_count": 2,
+      "unresolved_devices_count": 2,
+      "medical_devices_count": 1,
+      "iot_devices_count": 0,
+      "it_devices_count": 1,
+      "status": "Unresolved"
+         ***/
+
+        for ($i = 0; $i < 30; $i++) {
+            $obj = new stdClass;
+
+            $obj->id = $i;
+            $obj->alert_type_name = Generator::generate(' ', 3);
+            $obj->category = "Segmentation";
+            $obj->detected_time = $this->randomDateInRange(Carbon::now()->startOfYear(), Carbon::now())->format('Y-m-d H:i:s');
+            $obj->updated_time = $this->randomDateInRange(Carbon::now()->startOfYear(), Carbon::now())->format('Y-m-d H:i:s');
+            $obj->devices_count = random_int(1, 50);
+            $obj->unresolved_devices_count = random_int(1, 50);
+            $obj->medical_devices_count = random_int(1, 15);
+            $obj->iot_devices_count = random_int(0, 50);
+            $obj->it_devices_count = random_int(0, 8);
+            $obj->status = $status[array_rand($status)];
+
+            array_push($data, $obj);
+        }
+
+        return $this->paginate($data);
+    }
+
+    function randomDateInRange(DateTime $start, DateTime $end)
+    {
+        $randomTimestamp = mt_rand($start->getTimestamp(), $end->getTimestamp());
+        $randomDate = new DateTime();
+        $randomDate->setTimestamp($randomTimestamp);
+        return $randomDate;
+    }
+
+    public function paginate($items, $perPage = 5, $page = null, $options = [])
+    {
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Alerts $alerts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Alerts $alerts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Alerts $alerts)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/DeviceController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Device;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Str;
+use stdClass;
+
+class DeviceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = [];
+        $scores = ["Low", "Medium", "High"];
+
+        for ($i = 0; $i < 30; $i++) {
+            $vlan = array_map(
+                function () {
+                    return rand(0, 100);
+                },
+                array_fill(0, rand(0, 4), null)
+            );
+
+            $newObj = new stdClass;
+            $newObj->uid = Str::uuid();
+            $newObj->asset_id = Str::random(7);
+            $newObj->risk_score = $scores[array_rand($scores)];
+            $newObj->os_category = "Other";
+            $newObj->labels = [];
+            $newObj->device_type_family = "Printer";
+            $newObj->vlan_list = $vlan;
+            $newObj->mac_list = [
+                "0a:65:bc:43:38:97"
+            ];
+            $newObj->device_subcategory = "General IoT";
+            $newObj->retired = (bool)random_int(0, 1);
+            $newObj->assignees = [];
+            $newObj->network_list = ["Corporate"];
+            $newObj->model = "Laser Printer";
+            $newObj->device_type = "Printer";
+            $newObj->device_category = "IoT";
+            $newObj->ip_list = ["10.100.3.80"];
+
+            array_push($data, $newObj);
+        }
+
+        return $this->paginate($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Device $device)
+    {
+        //
+    }
+
+    public function paginate($items, $perPage = 5, $page = null, $options = [])
+    {
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Device $device)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Device $device)
+    {
+        //
+    }
+}
